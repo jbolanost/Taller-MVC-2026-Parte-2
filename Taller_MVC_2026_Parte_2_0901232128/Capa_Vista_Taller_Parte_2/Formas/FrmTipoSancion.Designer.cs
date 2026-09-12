68bc6dc1140a8fbccbda806f9cbc95005a5511ed
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-11 21:05:26 -0600

    Agregando Capa Vista Taller MVC Parte 2

---
+++ Taller_MVC_2026_Parte_2_0901232128/Capa_Vista_Taller_Parte_2/Formas/FrmTipoSancion.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Capa_Vista_Taller_Parte_2.Formas
+{
+    partial class FrmTipoSancion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTipoSancion = new System.Windows.Forms.DataGridView();
+            this.panDatos = new System.Windows.Forms.Panel();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.comboI1 = new Capa_Vista_Combo.ComboI();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoSancion)).BeginInit();
+            this.panDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvTipoSancion
+            // 
+            this.dgvTipoSancion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTipoSancion.Location = new System.Drawing.Point(29, 326);
+            this.dgvTipoSancion.Name = "dgvTipoSancion";
+            this.dgvTipoSancion.RowHeadersWidth = 51;
+            this.dgvTipoSancion.RowTemplate.Height = 24;
+            this.dgvTipoSancion.Size = new System.Drawing.Size(558, 193);
+            this.dgvTipoSancion.TabIndex = 0;
+            // 
+            // panDatos
+            // 
+            this.panDatos.Controls.Add(this.txtDescripcion);
+            this.panDatos.Controls.Add(this.txtNombre);
+            this.panDatos.Controls.Add(this.lblDescripcion);
+            this.panDatos.Controls.Add(this.lblNombre);
+            this.panDatos.Location = new System.Drawing.Point(12, 12);
+            this.panDatos.Name = "panDatos";
+            this.panDatos.Size = new System.Drawing.Size(611, 226);
+            this.panDatos.TabIndex = 1;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(17, 93);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(558, 117);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(17, 33);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(558, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(14, 72);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(82, 16);
+            this.lblDescripcion.TabIndex = 1;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(14, 14);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
+            this.btnNuevo.Location = new System.Drawing.Point(629, 12);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(143, 44);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click_1);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.Color.White;
+            this.btnBorrar.Location = new System.Drawing.Point(629, 130);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(143, 44);
+            this.btnBorrar.TabIndex = 3;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click_1);
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnAyuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAyuda.ForeColor = System.Drawing.Color.White;
+            this.btnAyuda.Location = new System.Drawing.Point(629, 476);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(143, 44);
+            this.btnAyuda.TabIndex = 4;
+            this.btnAyuda.Text = "Ayuda";
+            this.btnAyuda.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(629, 70);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(143, 44);
+            this.btnEditar.TabIndex = 5;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.Indigo;
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.ForeColor = System.Drawing.Color.White;
+            this.btnImprimir.Location = new System.Drawing.Point(629, 194);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(143, 44);
+            this.btnImprimir.TabIndex = 6;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.BackColor = System.Drawing.Color.DarkGray;
+            this.btnGrabar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.ForeColor = System.Drawing.Color.White;
+            this.btnGrabar.Location = new System.Drawing.Point(629, 376);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(143, 79);
+            this.btnGrabar.TabIndex = 7;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = false;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click_1);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(29, 258);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(558, 22);
+            this.txtBuscar.TabIndex = 4;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.White;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.Black;
+            this.btnBuscar.Location = new System.Drawing.Point(629, 258);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(143, 44);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // comboI1
+            // 
+            this.comboI1.Location = new System.Drawing.Point(29, 286);
+            this.comboI1.Name = "comboI1";
+            this.comboI1.Size = new System.Drawing.Size(573, 30);
+            this.comboI1.TabIndex = 9;
+            // 
+            // FrmTipoSancion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 532);
+            this.Controls.Add(this.comboI1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAyuda);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.panDatos);
+            this.Controls.Add(this.dgvTipoSancion);
+            this.Name = "FrmTipoSancion";
+            this.Text = "FrmTipoSancion";
+            this.Load += new System.EventHandler(this.FrmTipoSancion_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoSancion)).EndInit();
+            this.panDatos.ResumeLayout(false);
+            this.panDatos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTipoSancion;
+        private System.Windows.Forms.Panel panDatos;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private Capa_Vista_Combo.ComboI comboI1;
+    }
+}
---
+++ Taller_MVC_2026_Parte_2_0901232128/Capa_Vista_Taller_Parte_2/Formas/FrmTipoSancion.cs
@@ -0,0 +1,111 @@
+﻿using Capa_Controlador_Taller_Parte_2.Estados;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Controlador_Taller_Parte_2.Controlador;
+
+namespace Capa_Vista_Taller_Parte_2.Formas
+{
+    public partial class FrmTipoSancion : Form
+    {
+        private ModeloTipoSanciones tipo_sancion = new ModeloTipoSanciones();
+        public FrmTipoSancion()
+        {
+            InitializeComponent();
+            panDatos.Enabled = false;
+            CargarDatos();
+        }
+        private void listaTipoSancion()
+        {
+            try
+            {
+                dgvTipoSancion.DataSource = tipo_sancion.GetAll();
+            }
+            catch (Exception ex)
+            {
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+        private void Reinicio()
+        {
+            panDatos.Enabled = false;
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+        }
+
+        private void btnNuevo_Click_1(object sender, EventArgs e)
+        {
+            panDatos.Enabled = true;
+            tipo_sancion.Estado = EstadoEntidad.Added;
+        }
+
+        private void btnBorrar_Click_1(object sender, EventArgs e)
+        {
+            if (dgvTipoSancion.SelectedRows.Count > 0)
+            {
+                tipo_sancion.Estado = EstadoEntidad.Deleted;
+                tipo_sancion.id_tipo_sancion = Convert.ToInt32(dgvTipoSancion.CurrentRow.Cells[0].Value);
+                string resultado = tipo_sancion.GrabarCambios();
+                MessageBox.Show(resultado);
+                listaTipoSancion();
+            }
+            else MessageBox.Show("Seleccione una fila");
+        }
+
+        private void btnEditar_Click_1(object sender, EventArgs e)
+        {
+            if (dgvTipoSancion.SelectedRows.Count > 0)
+            {
+                panDatos.Enabled = true;
+                tipo_sancion.Estado = EstadoEntidad.Modified;
+                tipo_sancion.id_tipo_sancion = Convert.ToInt32(dgvTipoSancion.CurrentRow.Cells[0].Value);
+                txtNombre.Text = dgvTipoSancion.CurrentRow.Cells[1].Value.ToString();
+                txtDescripcion.Text = dgvTipoSancion.CurrentRow.Cells[2].Value.ToString();
+            }
+            else MessageBox.Show("Seleccione una fila");
+        }
+
+        private void btnGrabar_Click_1(object sender, EventArgs e)
+        {
+            tipo_sancion.nombre_tipo = txtNombre.Text;
+            tipo_sancion.descripcion = txtDescripcion.Text;
+            bool valido = new Ayudas.ValidacionDatos(tipo_sancion).Validar();
+            if (valido == true)
+            {
+                string resultado = tipo_sancion.GrabarCambios();
+                MessageBox.Show(resultado);
+                listaTipoSancion();
+                Reinicio();
+            }
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            dgvTipoSancion.DataSource = tipo_sancion.FindbyId(txtBuscar.Text);
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            dgvTipoSancion.DataSource = tipo_sancion.FindbyId(txtBuscar.Text);
+        }
+
+        private void FrmTipoSancion_Load_1(object sender, EventArgs e)
+        {
+            listaTipoSancion();
+        }
+
+        void CargarDatos()
+        {
+            comboI1.llenarCombo("sancion", "id_tipo_sancion", "descripcion");
+
+        }
+    }
+}
