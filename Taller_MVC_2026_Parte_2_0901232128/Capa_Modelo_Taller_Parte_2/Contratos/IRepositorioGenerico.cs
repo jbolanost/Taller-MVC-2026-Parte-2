fd83bb7bb20c731ccfb703124fd6c06d0bbf2946
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-11 20:56:25 -0600

    Agregando Capa Modelo Taller MVC Parte 2

---
+++ Taller_MVC_2026_Parte_2_0901232128/Capa_Modelo_Taller_Parte_2/Contratos/IRepositorioGenerico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo_Taller_Parte_2.Contratos
+{
+    public interface IRepositorioGenerico<Entity> where Entity : class
+    {
+        int Agregar(Entity entidad);
+        int Editar(Entity entidad);
+        int Remover(Entity entidad);
+        IEnumerable<Entity> GetAll();
+
+    }
+}
---
+++ Taller_MVC_2026_Parte_2_0901232128/Taller_MVC_2026_Parte_2_0901232128/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Taller_MVC_2026_Parte_2_0901232128")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Taller_MVC_2026_Parte_2_0901232128")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("9f685a2b-059a-4a22-95db-295c8e3457db")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
