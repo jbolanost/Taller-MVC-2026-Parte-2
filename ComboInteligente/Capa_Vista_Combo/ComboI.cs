5c1d87efef16677fa9d23ac6285f6a62cd1fbca0
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-11 21:10:09 -0600

    Agregando Proyecto Ejecutable Y ComboInteligente Taller MVC Parte 2

---
+++ ComboInteligente/Capa_Vista_Combo/ComboI.cs
@@ -0,0 +1,41 @@
+﻿using Capa_Controlador_Combo.Controlador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Combo
+{
+    public partial class ComboI : UserControl
+    {
+        public ComboI()
+        {
+            InitializeComponent();
+        }
+        ModeloCombo controlador = new ModeloCombo();
+
+        public void llenarCombo(string _tabla, string _campo1, string _campo2)
+        {
+
+            var dtTabla = controlador.enviarDatos(_tabla, _campo1, _campo2);
+
+            cboPrueba.DataSource = dtTabla;
+            cboPrueba.ValueMember = "id_tipo_sancion";
+            cboPrueba.DisplayMember = "descripcion";
+            AutoCompleteStringCollection coleccion = new AutoCompleteStringCollection();
+            foreach (DataRow row in dtTabla.Rows)
+            {
+                coleccion.Add(Convert.ToString(row[_campo1]) + "-" + Convert.ToString(row[_campo2]));
+                coleccion.Add(Convert.ToString(row[_campo2]) + "-" + Convert.ToString(row[_campo1]));
+            }
+            cboPrueba.AutoCompleteCustomSource = coleccion;
+            cboPrueba.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            cboPrueba.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+    }
+}
