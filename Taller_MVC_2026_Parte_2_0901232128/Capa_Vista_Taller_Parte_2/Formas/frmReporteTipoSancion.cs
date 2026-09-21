2f61405b8b94a9f58bb43e1d00dd4237e81695a3
Jose Xavier Bolaños Tenas <jbolanost@miumg.edu.gt>
2026-09-20 20:31:00 -0600

    Taller de Reportes en C#.

---
+++ Taller_MVC_2026_Parte_2_0901232128/Capa_Vista_Taller_Parte_2/Formas/frmReporteTipoSancion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Controlador_Taller_Parte_2;
+using Capa_Controlador_Taller_Parte_2.Controlador;
+using Microsoft.Reporting.WinForms;
+
+namespace Capa_Vista_Taller_Parte_2.Formas
+{
+    public partial class frmReporteTipoSancion : Form
+    {
+        private ModeloTipoSanciones tipoSanciones = new ModeloTipoSanciones();
+        public frmReporteTipoSancion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporteTipoSancion_Load(object sender, EventArgs e)
+        {
+            ReportDataSource reportDataSource = new ReportDataSource("ReporteTipoSanciones", tipoSanciones.GetAll());
+            rvTipoSanciones.LocalReport.ReportEmbeddedResource = "Capa_Vista_Taller_Parte_2.Reportes.ReporteTipoSanciones.rdlc";
+            rvTipoSanciones.LocalReport.DataSources.Clear();
+            rvTipoSanciones.LocalReport.DataSources.Add(reportDataSource);
+
+            this.rvTipoSanciones.RefreshReport();
+        }
+    }
+}
